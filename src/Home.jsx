5792fdc69c51982d70f81bbc5cd20b68bf46141c
christopherforsythe <christopherforsythe123@gmail.com>
2024-03-07 16:40:24 +0000

    lovejoy initial commit for landing page

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "./Assets/logo.jpg";
+
+const Home = () => {
+  return (
+    <div className="landing">
+      <img src={logo} alt="" className="logo" />
+      <h3><i>Coming May 2024</i></h3>
+    </div>
+  )
+}
+
+export default Home
